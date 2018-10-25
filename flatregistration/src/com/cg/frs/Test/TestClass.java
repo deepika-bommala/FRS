d21747f47789a42b02f140e3de32bd681852f440
deepika-bommala <44389255+deepika-bommala@users.noreply.github.com>
2018-10-25 18:07:07 +0530

    Add files via upload

---
+++ flatregistration/src/com/cg/frs/Test/TestClass.java
@@ -0,0 +1,15 @@
+package com.cg.frs.Test;
+
+import org.junit.Test;
+
+import com.cg.frs.service.IflatRegService;
+import com.cg.frs.service.flatRegServiceImpl;
+
+public class TestClass {
+	IflatRegService cliSer = new flatRegServiceImpl();
+	@Test
+	public void validatefArea1() {
+		
+	}
+
+}
